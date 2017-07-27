b4848d9564571c531c61f02acae95d3f9bccb7a0
刘跃明 <471025316@qq.com>
2017-07-27 12:39:57 +0800

    坐标系相关

--- Coordinate/README.md
+++ Coordinate/README.md
@@ -1,2 +1,51 @@
 ﻿#### 坐标系相关知识
-test
+
+__一.Android坐标系定义__  
+Android坐标系是以屏幕左上角为坐标原点，X轴向右增大，Y轴向下增大（与数字坐标系相反）  
+在坐标系根据两个点就可以确定一块矩形区域，即View的左上角和右下角2个坐标  
+1.相对于View的坐标系:即以View左上角为原点(0,0)的坐标系     
+2.相对于父View的坐标系:即以父View左上角为原点(0,0)的坐标系       
+3.相对于屏幕坐标系，绝对坐标:即以屏幕左上角为原点(0,0)的坐标系     
+总之相对坐标系，就是以某一个View的左上角作为原点的坐标系
+
+-------------
+__View相关坐标方法__  
+
+View获取坐标方法  | 坐标说明|
+--------- | --------|
+以下方法获取坐标都是相对于父布局|
+getLeft()  |返回View左上角X坐标 |
+getTop()  | 返回View左上角Y坐标 |
+getTop()  | 返回View右下角X坐标 |
+getTop()  | 返回View右下角Y坐标 |
+getX()  | 返回值为getLeft()+getTranslationX()，当setTranslationX()时getLeft()不变，getX()变。 |
+getY()  | 返回值为getTop()+getTranslationY()，当setTranslationY()时getTop()不变，getY()变。 |       
+
+__触摸事件相关__  
+
+MotionEvent坐标方法  | 坐标说明|
+--------- | --------|
+以下方法获取坐标都是相对于父布局|
+getX()  |当前触摸点X坐标 |
+getY()  | 当前触摸点Y坐标 |
+以下方法获取坐标都是相对于屏幕|
+getRawX()  | 当前触摸点X坐标 |
+getRawY()  | 当前触摸点Y坐标 |
+
+__View宽高相关方法__
+    
+View宽高方法  | 说明|
+--------- | --------|
+getWidth()  |layout后有效，返回值是mRight-mLeft，|
+getHeight()  | layout后有效，返回值是mBottom-mTop |
+getMeasuredWidth()  | 返回measure过程得到的mMeasuredWidth值 |
+getMeasuredHeight()  | 返回measure过程得到的mMeasuredHeight值|
+
+__View获取屏幕中位置坐标的相关方法__
+    
+View的方法  | 说明|
+--------- | --------|
+getLocalVisibleRect()  |获取相对于View坐标系，View的自身可见的区域的坐标，返回可见区域 左上角和右下角坐标|
+getGlobalVisibleRect()  | 获取相对于屏幕坐标系，View的自身可见的区域的坐标，返回可见区域 左上角和右下角坐标|
+getLocationOnScreen()  | 获取相对屏幕坐标系,View左上角坐标|
+getLocationInWindow()  | 普通Activity 相对屏幕坐标系;如果为对话框式的Activity则是相对于Activity坐标系；返回View的左上角坐标|
--- Coordinate/app/src/main/java/com/dqr/www/coordinate/MainActivity.java
+++ Coordinate/app/src/main/java/com/dqr/www/coordinate/MainActivity.java
@@ -1,14 +1,103 @@
 package com.dqr.www.coordinate;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int lastX;
+    int lastY;
 
+    private View mMoveView;
+    private TextView mTvCoordinate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		//test
+        mMoveView=findViewById(R.id.move_view);
+        mTvCoordinate = (TextView) findViewById(R.id.tv_coordinate);
+
+        mMoveView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int x = (int) event.getRawX();
+                int y = (int) event.getRawY();
+                switch (event.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN: {
+
+                        break;
+                    }
+
+                    case MotionEvent.ACTION_MOVE: {
+                        int left = x - lastX;
+                        int top = y - lastY;
+                        mMoveView.layout(mMoveView.getLeft()+left,mMoveView.getTop()+top,mMoveView.getRight()+left,mMoveView.getBottom()+top);
+                        mMoveView.postInvalidate();
+                        break;
+                    }
+
+                    case MotionEvent.ACTION_UP: {
+
+                        break;
+                    }
+
+                }
+                lastX = x;
+                lastY = y;
+
+                //获取View自身可见坐标
+                Rect localVisible = new Rect();
+                mMoveView.getLocalVisibleRect(localVisible);
+                //获取View相对屏幕可见区域坐标
+                Rect globalVisible = new Rect();
+                mMoveView.getGlobalVisibleRect(globalVisible);
+                //获取View左上角相对于屏幕坐标
+                int[] screen = new int[2];
+                mMoveView.getLocationOnScreen(screen);
+
+                //View左上角相对屏幕或者Dialog模式Activity 相对与标题栏左上角坐标
+                int[] locationInWindow = new int[2];
+                mMoveView.getLocationInWindow(locationInWindow);
+
+                String coordinateStr=
+                        "\n---------View坐标--------"
+                        +"\ngetLeft():"+mMoveView.getLeft()
+                        +"\ngetTop():"+mMoveView.getTop()
+                        +"\ngetRight():"+mMoveView.getRight()
+                        +"\ngetBottom():"+mMoveView.getBottom()
+                        +"\ngetX():"+mMoveView.getX()
+                        +"\ngetY():"+mMoveView.getY()
+                        +"\n-------触摸事件坐标--------"
+                        +"\nMotionEvent.getX():"+event.getX()
+                        +"\nMotionEvent.getY():"+event.getY()
+                        +"\nMotionEvent.getRawX():"+event.getRawX()
+                        +"\nMotionEvent.getRawY():"+event.getRawY()
+                        +"\n--------获取View自身可见坐标--------"
+                        +"\ngetLocalVisibleRect.left:"+localVisible.left
+                        +"\ngetLocalVisibleRect.top:"+localVisible.top
+                        +"\ngetLocalVisibleRect.right:"+localVisible.right
+                        +"\ngetLocalVisibleRect.bottom:"+localVisible.bottom
+                        +"\n---------获取View相对屏幕可见区域坐标--------"
+                        +"\ngetGlobalVisibleRect.left:"+globalVisible.left
+                        +"\ngetGlobalVisibleRect.top:"+globalVisible.top
+                        +"\ngetGlobalVisibleRect.right:"+globalVisible.right
+                        +"\ngetGlobalVisibleRect.bottom:"+globalVisible.bottom
+                        +"\n----------获取View左上角相对于屏幕坐标--------"
+                        +"\ngetLocationOnScreen.x:"+screen[0]
+                        +"\ngetLocationOnScreen.y:"+screen[1]
+                        +"\n----------View左上角相对屏幕或者Dialog模式Activity 相对与标题栏左上角坐标-------"
+                        +"\ngetLocationInWindow.x:"+locationInWindow[0]
+                        +"\ngetLocationInWindow.y:"+locationInWindow[1];
+
+
+                mTvCoordinate.setText(coordinateStr);
+                return true;
+            }
+        });
+
     }
 }
