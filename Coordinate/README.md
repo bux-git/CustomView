fb4d7bc71a3bdbff139d70389752d650a174553d
刘跃明 <471025316@qq.com>
2017-07-27 16:24:08 +0800

    坐标系相关

--- Coordinate/README.md
+++ Coordinate/README.md
@@ -1,12 +1,37 @@
 ﻿#### 坐标系相关知识
 
 __一.Android坐标系定义__  
+![img](https://github.com/bux-git/CustomView/raw/master/Coorinate/coordinate.gif)   
+
 Android坐标系是以屏幕左上角为坐标原点，X轴向右增大，Y轴向下增大（与数字坐标系相反）  
-在坐标系根据两个点就可以确定一块矩形区域，即View的左上角和右下角2个坐标  
-1.相对于View的坐标系:即以View左上角为原点(0,0)的坐标系     
-2.相对于父View的坐标系:即以父View左上角为原点(0,0)的坐标系       
-3.相对于屏幕坐标系，绝对坐标:即以屏幕左上角为原点(0,0)的坐标系     
-总之相对坐标系，就是以某一个View的左上角作为原点的坐标系
+在坐标系中根据两个点就可以确定一块矩形区域，即View的左上角和右下角2个坐标可以确定View的大小  
+* 1.相对于View的坐标系:即以View左上角为原点(0,0)的坐标系      
+* 2.相对于父View的坐标系:即以父View左上角为原点(0,0)的坐标系       
+* 3.相对于屏幕坐标系，绝对坐标:即以屏幕左上角为原点(0,0)的坐标系     
+* 总之相对坐标系，就是以某一个View的左上角作为原点的坐标系
+
+一般来说，我们要获取 View 的坐标和高度 等，都必须等到 View 绘制完毕以后才能获取的到，   
+在 Activity 的 onCreate() 方法 里面 是获取不到的，必须等到 View 绘制完毕以后才能获取地到 View 的响应的坐标，    
+一般来说，主要有以下两种方法： 
+>    第一种方法，Activity onWindowFocusChanged() 方法里面进行调用
+
+    @Override
+     public void onWindowFocusChanged(boolean hasFocus) {
+         super.onWindowFocusChanged(hasFocus);
+         //确保只执行一次
+         if (first && hasFocus) {
+             first=true;
+         }
+     }
+>第二种方法，在视图树绘制完成的时候进行测量：
+
+    mMoveView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                //执行一次之后移除
+                mMoveView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
 
 -------------
 __View相关坐标方法__  
@@ -18,6 +43,9 @@ getLeft()  |返回View左上角X坐标 |
 getTop()  | 返回View左上角Y坐标 |
 getTop()  | 返回View右下角X坐标 |
 getTop()  | 返回View右下角Y坐标 |
+在 api 14 以后 ，在动画执行过程中，要改变 View 的状态，推荐使用 setTranslationY() 和 setTranslationX() 等方法，而尽量避免改变 LayoutParams.因为性能来说较差|
+getTranslationX()  | 返回值为X方向上相对与Left的偏移量 默认值为0| 
+getTranslationY()  | 返回值为Y方向上相对于top的偏移量 默认值为0| 
 getX()  | 返回值为getLeft()+getTranslationX()，当setTranslationX()时getLeft()不变，getX()变。 |
 getY()  | 返回值为getTop()+getTranslationY()，当setTranslationY()时getTop()不变，getY()变。 |       
 
@@ -49,3 +77,18 @@ getLocalVisibleRect()  |获取相对于View坐标系，View的自身可见的区
 getGlobalVisibleRect()  | 获取相对于屏幕坐标系，View的自身可见的区域的坐标，返回可见区域 左上角和右下角坐标|
 getLocationOnScreen()  | 获取相对屏幕坐标系,View左上角坐标|
 getLocationInWindow()  | 普通Activity 相对屏幕坐标系;如果为对话框式的Activity则是相对于Activity坐标系；返回View的左上角坐标|
+
+__View滑动相关的坐标__    
+
+ View的滑动方法  | 说明|
+ --------- | --------|
+ offsetLeftAndRight(int offset) |X方向挪动View，offset为正则x轴正向移动，移动的是整个View，getLeft()会变的|
+ offsetTopAndBottom(int offset)  | Y向挪动View，offset为正则y轴正向移动，移动的是整个View，getTop()会变的 |
+  偏移量mScrollX，mScrollY只针对于该View中onDraw()方法里的内容|
+   mScrollX | 该View的内容相对于View坐标系X方向的偏移量|
+   mScrollY | 该View的内容相对于View坐标系Y方向的偏移量|
+ scrollTo(int x, int y)  |将View中的内容在相对于View的坐标系中，滑动偏移相应的x,y 注意：x，y为正时向靠近原点的方向偏移 为负则相反.同时此类滑动方法值改变 mScrollX Y的值 不该变top left等|
+scrollBy(int x, int y) | 实质为scrollTo()，只是只改变Y轴滑动。|
+setScrollX(int value) | 在scrollTo()的基础上继续滑动xy。|
+setScrollY(int value) | 实质为scrollTo()，只是只改变X轴滑动。|
+getScrollX()/getScrollY() | 获取当前滑动位置偏移量。|
--- Coordinate/app/src/main/java/com/dqr/www/coordinate/MainActivity.java
+++ Coordinate/app/src/main/java/com/dqr/www/coordinate/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,14 +14,16 @@ public class MainActivity extends AppCompatActivity {
 
     private View mMoveView;
     private TextView mTvCoordinate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMoveView=findViewById(R.id.move_view);
+        mMoveView = findViewById(R.id.move_view);
         mTvCoordinate = (TextView) findViewById(R.id.tv_coordinate);
 
         mMoveView.setOnTouchListener(new View.OnTouchListener() {
+
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 int x = (int) event.getRawX();
@@ -35,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
                     case MotionEvent.ACTION_MOVE: {
                         int left = x - lastX;
                         int top = y - lastY;
-                        mMoveView.layout(mMoveView.getLeft()+left,mMoveView.getTop()+top,mMoveView.getRight()+left,mMoveView.getBottom()+top);
+                        mMoveView.layout(mMoveView.getLeft() + left, mMoveView.getTop() + top, mMoveView.getRight() + left, mMoveView.getBottom() + top);
                         mMoveView.postInvalidate();
                         break;
                     }
@@ -63,41 +66,63 @@ public class MainActivity extends AppCompatActivity {
                 int[] locationInWindow = new int[2];
                 mMoveView.getLocationInWindow(locationInWindow);
 
-                String coordinateStr=
+                String coordinateStr =
                         "\n---------View坐标--------"
-                        +"\ngetLeft():"+mMoveView.getLeft()
-                        +"\ngetTop():"+mMoveView.getTop()
-                        +"\ngetRight():"+mMoveView.getRight()
-                        +"\ngetBottom():"+mMoveView.getBottom()
-                        +"\ngetX():"+mMoveView.getX()
-                        +"\ngetY():"+mMoveView.getY()
-                        +"\n-------触摸事件坐标--------"
-                        +"\nMotionEvent.getX():"+event.getX()
-                        +"\nMotionEvent.getY():"+event.getY()
-                        +"\nMotionEvent.getRawX():"+event.getRawX()
-                        +"\nMotionEvent.getRawY():"+event.getRawY()
-                        +"\n--------获取View自身可见坐标--------"
-                        +"\ngetLocalVisibleRect.left:"+localVisible.left
-                        +"\ngetLocalVisibleRect.top:"+localVisible.top
-                        +"\ngetLocalVisibleRect.right:"+localVisible.right
-                        +"\ngetLocalVisibleRect.bottom:"+localVisible.bottom
-                        +"\n---------获取View相对屏幕可见区域坐标--------"
-                        +"\ngetGlobalVisibleRect.left:"+globalVisible.left
-                        +"\ngetGlobalVisibleRect.top:"+globalVisible.top
-                        +"\ngetGlobalVisibleRect.right:"+globalVisible.right
-                        +"\ngetGlobalVisibleRect.bottom:"+globalVisible.bottom
-                        +"\n----------获取View左上角相对于屏幕坐标--------"
-                        +"\ngetLocationOnScreen.x:"+screen[0]
-                        +"\ngetLocationOnScreen.y:"+screen[1]
-                        +"\n----------View左上角相对屏幕或者Dialog模式Activity 相对与标题栏左上角坐标-------"
-                        +"\ngetLocationInWindow.x:"+locationInWindow[0]
-                        +"\ngetLocationInWindow.y:"+locationInWindow[1];
+                                + "\ngetLeft():" + mMoveView.getLeft()
+                                + "\ngetTop():" + mMoveView.getTop()
+                                + "\ngetRight():" + mMoveView.getRight()
+                                + "\ngetBottom():" + mMoveView.getBottom()
+                                + "\ngetX():" + mMoveView.getX()
+                                + "\ngetY():" + mMoveView.getY()
+                                + "\n-------触摸事件坐标--------"
+                                + "\nMotionEvent.getX():" + event.getX()
+                                + "\nMotionEvent.getY():" + event.getY()
+                                + "\nMotionEvent.getRawX():" + event.getRawX()
+                                + "\nMotionEvent.getRawY():" + event.getRawY()
+                                + "\n--------获取View自身可见坐标--------"
+                                + "\ngetLocalVisibleRect.left:" + localVisible.left
+                                + "\ngetLocalVisibleRect.top:" + localVisible.top
+                                + "\ngetLocalVisibleRect.right:" + localVisible.right
+                                + "\ngetLocalVisibleRect.bottom:" + localVisible.bottom
+                                + "\n---------获取View相对屏幕可见区域坐标--------"
+                                + "\ngetGlobalVisibleRect.left:" + globalVisible.left
+                                + "\ngetGlobalVisibleRect.top:" + globalVisible.top
+                                + "\ngetGlobalVisibleRect.right:" + globalVisible.right
+                                + "\ngetGlobalVisibleRect.bottom:" + globalVisible.bottom
+                                + "\n----------获取View左上角相对于屏幕坐标--------"
+                                + "\ngetLocationOnScreen.x:" + screen[0]
+                                + "\ngetLocationOnScreen.y:" + screen[1]
+                                + "\n----------View左上角相对屏幕或者Dialog模式Activity 相对与标题栏左上角坐标-------"
+                                + "\ngetLocationInWindow.x:" + locationInWindow[0]
+                                + "\ngetLocationInWindow.y:" + locationInWindow[1];
 
 
                 mTvCoordinate.setText(coordinateStr);
+               // mTvCoordinate.scrollTo(100,0);
                 return true;
             }
         });
 
+        mMoveView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                //执行一次之后移除
+                mMoveView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
+
+
+    }
+
+    boolean first = true;
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        //确保只执行一次
+        if (first && hasFocus) {
+            first=true;
+
+        }
     }
 }
